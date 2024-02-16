1941878db36eca946bf3f706b8d23180c42e469f
Miguel Angel Jimenez <miguejm@outlook.es>
2024-02-16 16:57:24 -0600

    change -> sign-in and sign-out methods added

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class Controller extends AbstractController
 {
@@ -24,5 +26,15 @@ class Controller extends AbstractController
 		public function decodeJWT(string $jwt): object
 		{
 			return JWT::decode($jwt, new Key($_ENV['JWT_KEY'] ?? '', $_ENV['JWT_ALG'] ?? ''));
+		}
+
+		/**
+		 * @throws BadRequestHttpException undefined **authorization token**
+		 */
+		public function getAuthorizationToken(Request $request): string
+		{
+			$authorization = $request->headers->get('Authorization');
+			if(!$authorization) throw new BadRequestHttpException("Bad request(undefined authorization token).", null, 400);
+			return substr($authorization, 5);
 		}
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,36 +2,95 @@
 
 namespace App\Controller;
 
+use App\Entity\Salt;
+use App\Entity\Session;
 use App\Entity\User;
 use App\Entity\UserCredential;
+use App\Exception\NotFoundEmailException;
+use App\Exception\NotFoundPasswordException;
+use App\Exception\NotFoundSaltException;
 use App\Repository\SaltRepository;
+use App\Repository\SessionRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/', name: 'security')]
 class SecurityController extends Controller
 {
+    /**
+     * @throws NotFoundEmailException
+     * @throws NotFoundPasswordException
+     */
     #[Route('/sign-in', name: '_sign_in', methods: ['POST'])]
-    public function signIn(Request $request, UserRepository $userRepository): Response
+    public function signIn(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $userCredential = UserCredential::fromArray(json_decode($request->getContent(), true));
+        $userRepository = $entityManager->getRepository(User::class);
+        $searchUser = $userRepository->findOneBy(['email' => $userCredential->getEmail()]);
+        if(!$searchUser) throw new NotFoundEmailException($entityManager);
+        $encrytPassword = $this->encryptPassword($entityManager, $userCredential->getPassword(), $searchUser);
+        $userCredential->setPassword($encrytPassword);
+        $searchUser = $userRepository->findOneBy($userCredential->toArray());
+        if(!$searchUser) throw new NotFoundPasswordException($entityManager);
+        $sessionRepository = $entityManager->getRepository(Session::class);
+        $session = $this->createSession($sessionRepository, $searchUser);
+        $jwt = $this->generateJWT(['key' => $session->getUuid()]);
+        return $this->json(['authorization' => $jwt]);
+    }
 
+    #[Route('/sign-out', name: 'sign_out', methods: ['GET'])]
+    public function signOut(Request $request, SessionRepository $sessionRepository): Response
+    {
+        $jwt = $request->query->get('authorization');
+        if(!$jwt){
+            $jwt = $this->getAuthorizationToken($request);
+        }
+        $payload = $this->decodeJWT($jwt);
+        $uuidSession = $payload->data->key ?? null;
+        if(!$uuidSession) throw new UnauthorizedHttpException('', "Unauthorized (invalid authorization token.)");
+        $searchSession = $sessionRepository->findOneBy(['uuid' => $uuidSession]);
+        if(!$searchSession) throw new UnauthorizedHttpException('', "Unauthorized (invalid authorization token.)");
+        $searchSession->setActive(false);
+        $sessionRepository->update($searchSession);
         return $this->json([]);
     }
 
+    /**
+     * @throws NotFoundSaltException
+     */
     static function encryptPassword(
-        SaltRepository $saltRepository,
+        EntityManagerInterface $entityManager,
         string $password,
         ?User $user = null
     ): string
     {
         $salt = bin2hex(random_bytes(4));
         if($user){
+            $saltRepository = $entityManager->getRepository(Salt::class);
             $criteria = ['useri' => $user->getId()];
             $searchSalt = $saltRepository->findOneBy($criteria);
-            if(!$searchSalt);
+            if(!$searchSalt) throw new NotFoundSaltException($entityManager);
+            $salt = $searchSalt->getSalt();
         }
-        return '';
+        $fullPassword = $salt . $password;
+        return hash($_ENV['ALG'], $fullPassword);
+    }
+
+    private function createSession(SessionRepository $sessionRepository, User $user): Session
+    {
+        $uuid = Uuid::uuid4()->toString();
+        $expired = time() + (4 * 3600);
+        $session = new Session();
+        $session->setUseri($user->getId());
+        $session->setUuid($uuid);
+        $session->setActive(true);
+        $session->setExpired($expired);
+        $sessionRepository->create($session);
+        return $session;
     }
 }
