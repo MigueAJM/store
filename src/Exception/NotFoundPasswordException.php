fb85ef9bf255a2e00e990a9e091ae42e884a1426
Miguel Angel Jimenez <miguejm@outlook.es>
2024-02-16 16:56:52 -0600

    add -> email, password not found exception added

---
+++ src/Exception/NotFoundPasswordException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Exception;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+
+final class NotFoundPasswordException extends AbstractException
+{
+	public function __construct(EntityManagerInterface $entityManager)
+	{
+		parent::__construct($entityManager, Response::$statusTexts[404], Response::HTTP_NOT_FOUND, ErrorCode::NOT_FOUND_SALT);
+	}
+}
