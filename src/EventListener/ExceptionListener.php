7718cf621dfb0fe96211645e6536e3f642dc06e4
Miguel Angel Jimenez M <miguejm@outlook.es>
2024-02-16 22:00:00 -0600

    fix -> no validate httpCode

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -9,17 +9,18 @@ class ExceptionListener
     public function __invoke(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
+				$code = $exception->getCode() <= 99 ? 500 : $exception->getCode();
 				$payload = [
 					'error' => [
 						'exception' => end(explode("\\", get_class($exception))),
-						'httpCode' => $exception->getCode(),
+						'httpCode' => $code,
 						'message' => $exception->getMessage()
 					]
 				];
 				if(method_exists($exception, "getErrorCode")){
 					$payload['error']['code'] = $exception->getErrorCode();
 				}
-				$response = new JsonResponse($payload, $exception->getCode());
+				$response = new JsonResponse($payload, $code);
         $event->setResponse($response);
     }
 }
