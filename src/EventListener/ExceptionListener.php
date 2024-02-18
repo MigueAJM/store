c34c98f431efd01f08119714f1c72228b07d8aa6
Miguel Angel Jimenez M <miguejm@outlook.es>
2024-02-18 09:48:37 -0600

    fix -> end method

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -10,15 +10,20 @@ class ExceptionListener
     {
         $exception = $event->getThrowable();
 				$code = $exception->getCode() <= 99 ? 500 : $exception->getCode();
+				$explodeClass = explode("\\", get_class($exception));
+				$className = isset($explodeClass[1]) ? end($explodeClass) : $explodeClass[0];
 				$payload = [
 					'error' => [
-						'exception' => end(explode("\\", get_class($exception))),
+						'exception' => $className,
 						'httpCode' => $code,
 						'message' => $exception->getMessage()
 					]
 				];
 				if(method_exists($exception, "getErrorCode")){
 					$payload['error']['code'] = $exception->getErrorCode();
+				}
+				if($code === 500 && $_ENV['APP_ENV'] != "dev"){
+					$payload['error']['message'] = "Internal server error.";
 				}
 				$response = new JsonResponse($payload, $code);
         $event->setResponse($response);
