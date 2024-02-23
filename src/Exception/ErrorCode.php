bfd1e19c59388099f0ab897a7e06754b1035073a
Miguel Angel Jimenez M <miguejm@outlook.es>
2024-02-22 23:18:18 -0600

    change -> new codes
    NOT_FOUND_UUID_SESSION and EXPIRED_SESSION added

--- src/Exception/ErrorCode.php
+++ src/Exception/ErrorCode.php
@@ -7,4 +7,6 @@ final class ErrorCode
 	const NOT_FOUND_SALT = 1001;
 	const NOT_FOUND_EMAIL = 1002;
 	const NOT_FOUND_PASSWORD = 1003;
+	const NOT_FOUND_UUID_SESSION = 1004;
+	const EXPIRED_SESSION = 1005;
 }
