45f41700586a6f6ad4784d16bdfac49168fd1f3b
Miguel Angel Jimenez M <miguejm@outlook.es>
2024-02-22 23:17:22 -0600

    change -> public => static methods and new method
    getAuthenticatedUser method added

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Session;
+use App\Entity\User;
+use App\Exception\ExpiredSessionException;
+use App\Exception\NotFoundSessionException;
+use App\Interface\JsonWebTokenInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class Controller extends AbstractController
 {
-    public function generateJWT(mixed $payload): string
+    static function generateJWT(mixed $payload): string
 		{
 			$currentTimestamp = time();
 			$payload = [
@@ -25,18 +30,35 @@ class Controller extends AbstractController
 			return JWT::encode($payload, $_ENV['JWT_KEY'], $_ENV['JWT_ALG']);
 		}
 
-		public function decodeJWT(string $jwt): object
+		static function decodeJWT(string $jwt): JsonWebTokenInterface
 		{
-			return JWT::decode($jwt, new Key($_ENV['JWT_KEY'] ?? '', $_ENV['JWT_ALG'] ?? ''));
+			$payload = JWT::decode($jwt, new Key($_ENV['JWT_KEY'] ?? '', $_ENV['JWT_ALG'] ?? ''));
+			return new JsonWebTokenInterface((array)$payload);
 		}
 
 		/**
 		 * @throws UnauthorizedHttpException undefined **authorization token**
 		 */
-		public function getAuthorizationToken(Request $request): string
+		static function getAuthorizationToken(Request $request): string
 		{
 			$authorization = $request->headers->get('Authorization');
 			if(!$authorization) throw new UnauthorizedHttpException('', "Undefined authorization token.", null, Response::HTTP_UNAUTHORIZED);
 			return substr($authorization, 5);
+		}
+
+		/**
+		 * @throws NotFoundSessionException
+		 */
+		static public function getAuthenticatedUser(Request $request, EntityManagerInterface $entityManagerInterface): User
+		{
+			$jwt = self::getAuthorizationToken($request);
+			$payload = self::decodeJWT($jwt);
+			$uuidSession = $payload->data->key;
+			$sessionRepository = $entityManagerInterface->getRepository(Session::class);
+			$sessionActive = $sessionRepository->findOneBy(['uuid' => $uuidSession]);
+			if(!$sessionActive) throw new NotFoundSessionException($entityManagerInterface);
+			$now = time();
+			if(!$sessionActive->getActive() || $now > $sessionActive->getExpired()) throw new ExpiredSessionException($entityManagerInterface);
+			return $sessionActive->getUseri();
 		}
 }
